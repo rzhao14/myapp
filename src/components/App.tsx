83ac4c1b6dc08e1d207288802829077373c515b7
Rui Zhao <rzhao@intrepidcs.com>
2021-01-10 19:30:23 -0500

    create order with list of buyerId

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react'
 import DateTimePicker from 'react-time-picker';
 import DateBox from 'devextreme-react/date-box'
 import DropDownButton from 'devextreme-react/drop-down-button'
-import DataGrid, { Column } from 'devextreme-react/data-grid'
+import DataGrid, { Column, Editing } from 'devextreme-react/data-grid'
 import 'devextreme/dist/css/dx.common.css'
 import 'devextreme/dist/css/dx.light.css'
 import ResultsDisplay from './ResultsDisplay'
-import {getOrderById, getOrderByName, getRecentOrders} from '../services/APIServices'
+import {getOrderById, getOrderByName, getRecentOrders, getOrderTotal,getOrderItemByName, generateOrderWithNameAndModels} from '../services/APIServices'
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -15,10 +15,15 @@ function App() {
 
     const [orderId, setOrderId] = useState<string>('')
     const [orderName, setOrderName] = useState<string>('')
+    const [proposedOrderName, setProposedOrderName] = useState<string>('')
+    const [modelList, setModelList] = useState<string>('')
+    const [selectedOrder, setSelectedOrder] = useState<string>('')
     const [resultJson, setResultJson] = useState<any[]>([])
     const [recentOrders, setRecentOrders] = useState<any[]>([])
   useEffect(() => {
     getRecentOrders().then(data => setRecentOrders(data))
+    getOrderTotal().then(data => {
+    setProposedOrderName('O'+(data['COUNT(*)']+1))})
   }, [])
 
 
@@ -27,20 +32,53 @@ function App() {
             getOrderById(orderId).then(data => setResultJson(data))
         }
         else if(orderName!==''){
-            getOrderByName(orderName).then(data => setResultJson(data))
+            getOrderItemByName(orderName).then(data => setResultJson(data))
         }
     }
+    function handleGenerate(){
+        getOrderByName(proposedOrderName).then(data => {
+            if(data.length!==0){
+                window.alert(proposedOrderName + ' already taken. Please give a different name')
+            }else{
+                let list = modelList.trim().split(/[ \(,\)\n]+/)
+                let r = window.confirm('creating order with the following items:\n' + list.toString());
+                if(r){
+                    generateOrderWithNameAndModels({list:list, name:proposedOrderName}).then(data => {
+                        getOrderItemByName(proposedOrderName).then(data => setResultJson(data))
+                    })
+                }
+            }
+
+        })
+
+    }
     function handleSelect(value){
+      setSelectedOrder(value)
       if(value!==''){
         let orderInfo = value.split(':');
         if(orderInfo[0]!=='null'){
             getOrderById(orderInfo[0]).then(data => setResultJson(data))
         }else{
-            getOrderByName(orderInfo[1]).then(data => setResultJson(data))
+            getOrderItemByName(orderInfo[1]).then(data => setResultJson(data))
         }
       }
     }
-
+    function getPriceC(rowData){
+        if(rowData.priceC){
+            return rowData.priceC
+        }if(rowData.priceCI){
+            return rowData.priceCI
+        }
+        return ''
+    }
+    function getPriceU(rowData){
+        if(rowData.priceU){
+            return rowData.priceU
+        }if(rowData.priceUI){
+            return rowData.priceUI
+        }
+        return ''
+    }
 
   return (
       <>
@@ -50,48 +88,67 @@ function App() {
                  <div className='panel panel-primary'>
                    <div className='panel-heading'>Search</div>
                    <div className='panel-body'>
-                     <div className='col-md-2'>
+                     <div className='col-md-2 column'>
                        <label>Order Number</label>
+                        <br/>
                         <input type='text'
                             onChange={e => setOrderId(e.target.value)}
                         />
                      </div>
-                     <div className='col-md-2'>
+                     <div className='col-md-2 column'>
                         <label>Order Name</label>
+                         <br/>
                         <input type='text'
                             onChange={e => setOrderName(e.target.value)}
                         />
                      </div>
 
-                   <>
-                     <h4>Recent Orders</h4>
-                     <input
-                       list='orders'
-                       className='collections'
-                       onChange={e => handleSelect(e.target.value)}
-                     />
-                     <datalist id='orders'>
-                       {recentOrders.map((order) => (
-                         <option key={order.order_id}>{order.order_id +':'+ order.order_name  }</option>
-                       ))}
-                     </datalist>
-                   </>
-
-                     <div className='col-md-4'>
-                       <label>Data Range</label>
-                       <DateBox
-                         placeholder='Start Date'
-                         type='datetime'
-                         showClearButton={true}
-                         displayFormat='yyyy/MM/dd HH:mm:ss'
-                       />
-                       <DateBox
-                         placeholder='End Date'
-                         type='datetime'
-                         showClearButton={true}
-                         displayFormat='yyyy/MM/dd HH:mm:ss'
-                       />
+                     <div className='col-md-2 column'>
+                        <label>Recent Orders</label>
+                        <br/>
+                         <input
+                           list='orders'
+                           className='collections'
+                           value={selectedOrder}
+                           onChange={e => handleSelect(e.target.value)}
+                         />
+                         <datalist id='orders'>
+                           {recentOrders.map((order) => (
+                             <option key={order.order_id}>{order.order_id +':'+ order.order_name  }</option>
+                           ))}
+                         </datalist>
+                         <button
+                        id='searchButton'
+                        className='btn btn-success'
+                        type='submit'
+                        onClick={e=>setSelectedOrder('')}
+                      >
+                        <span className='glyphicons glyphicons-clear'></span>
+                        Clear
+                      </button>
                      </div>
+
+                     <div className='col-md-3'>
+                         <label>Models</label>
+                        <textarea
+                             className='model-input'
+                            onChange={e => setModelList(e.target.value)}
+                        />
+                         <button
+                            id='searchButton'
+                            className='btn btn-success'
+                            type='submit'
+                            onClick={handleGenerate}
+                            disabled={modelList===''}
+                          >
+                            <span className='glyphicons glyphicons-clear'></span>
+                            Generate Order
+                          </button>
+                          <input type='text' value={proposedOrderName} onChange={e => setProposedOrderName(e.target.value)}/>
+                      </div>
+
+
+
                    </div>
                    <div className='panel-footer'>
                      <div className='btn-bar'>
@@ -106,6 +163,16 @@ function App() {
                            <span className='glyphicons glyphicons-search'></span>
                            Search
                          </button>
+
+                         <button
+                            id='searchButton'
+                            className='btn btn-success'
+                            type='submit'
+                            onClick={handleSubmit}
+                          >
+                            <span className='glyphicons glyphicons-search'></span>
+                            Update Order Number
+                          </button>
                        </span>
                      </div>
                    </div>
@@ -123,17 +190,18 @@ function App() {
                          rowAlternationEnabled={true}
                          columnAutoWidth={true}
                        >
-                         <Column dataField='buyer_id' width={100} caption='Customer Model' />
-                         <Column dataField='oem_id' caption='OEM Model' />
-                         <Column dataField='factory_id' caption='Factory Model' />
-                         <Column dataField='x' caption='Length' />
-                         <Column dataField='y' caption='Width' />
-                         <Column dataField='z' caption='Height' />
-                         <Column dataField='description' caption='Description' />
-                         <Column dataField='quantity' caption='Quantity' />
-                         <Column dataField='priceC' caption='Unit Price CNY' />
-                         <Column dataField='priceU' caption='unit Price USD' />
-                         <Column dataField='quantity' caption='Quantity' />
+                       <Editing mode="row" allowUpdating={true} />
+                         <Column allowSorting={true} dataField='buyer_id' width={100} caption='Customer Model' />
+                         <Column allowSorting={true} dataField='oem_id' caption='OEM Model' />
+                         <Column allowSorting={true} dataField='factory_id' caption='Factory Model' />
+                         <Column allowSorting={true} dataField='x' caption='Length' />
+                         <Column allowSorting={true} dataField='y' caption='Width' />
+                         <Column allowSorting={true} dataField='z' caption='Height' />
+                         <Column allowSorting={true} dataField='description' caption='Description' />
+                         <Column allowSorting={true} dataField='quantity' caption='Quantity' />
+                         <Column allowSorting={true} calculateCellValue={getPriceC} caption='Unit Price CNY' />
+                         <Column allowSorting={true} calculateCellValue={getPriceU} caption='unit Price USD' />
+                         <Column allowSorting={true} dataField='quantity' caption='Quantity' />
 
                        </DataGrid>
                  )}
--- src/services/APIServices.tsx
+++ src/services/APIServices.tsx
@@ -4,13 +4,33 @@ export function getOrderById( order_id ) {
     method: 'get',
   }).then(res => res.json())
 }
-export function getOrderByName( order_name ) {
+export function getOrderTotal() {
+  return fetch('http://localhost:3000/ordersTotal', {
+    method: 'get',
+  }).then(res => res.json())
+}
+export function getOrderItemByName( order_name ) {
   return fetch('http://localhost:3000/orderItem/name/'+order_name, {
+    method: 'get',
+  }).then(res => res.json())
+}
+export function getOrderByName( order_name ) {
+  return fetch('http://localhost:3000/orders/name/'+order_name, {
     method: 'get',
   }).then(res => res.json())
 }
 export function getRecentOrders() {
   return fetch('http://localhost:3000/orders', {
     method: 'get',
+  }).then(res => res.json())
+}
+export function generateOrderWithNameAndModels(queryJsonString) {
+  return fetch('http://localhost:3000/orders', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(queryJsonString),
   }).then(res => res.json())
 }
